12d4251df06b9cea17c7719c38a0733e4181c27c
Julian Orth <ju.orth@gmail.com>
2015-05-15 21:05:19 +0200

    autocommit 2015-05-15 21:05:19 CEST

---
+++ src/str_one/cmp.rs
@@ -0,0 +1,166 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use core::ops::{Eq};
+use base::rmo::{AsRef};
+use byte_str::{ByteStr};
+use c_str::{CStr};
+use no_null_str::{NoNullStr};
+
+#[inline(always)] fn bsdata(b:  &ByteStr)   -> &[u8] { b.as_ref() }
+#[inline(always)] fn csdata(b:  &CStr)      -> &[u8] { b.as_ref() }
+#[inline(always)] fn nnsdata(b: &NoNullStr) -> &[u8] { b.as_ref() }
+
+#[inline(always)]
+fn csnndata(b: &CStr) -> &[u8] {
+    let data = csdata(b);
+    &data[..data.len()-1]
+}
+
+// ByteStr == ByteStr
+impl Eq for ByteStr {
+    fn eq(&self, other: &ByteStr) -> bool {
+        bsdata(self) == bsdata(other)
+    }
+}
+
+// ByteStr == CStr
+impl Eq<CStr> for ByteStr {
+    fn eq(&self, other: &CStr) -> bool {
+        bsdata(self) == csnndata(other)
+    }
+}
+
+// ByteStr == NoNullStr
+impl Eq<NoNullStr> for ByteStr {
+    fn eq(&self, other: &NoNullStr) -> bool {
+        bsdata(self) == nnsdata(other)
+    }
+}
+
+// ByteStr == str
+impl Eq<str> for ByteStr {
+    fn eq(&self, other: &str) -> bool {
+        bsdata(self) == other.as_bytes()
+    }
+}
+
+// str == ByteStr
+impl Eq<ByteStr> for str {
+    fn eq(&self, other: &ByteStr) -> bool {
+        self.as_bytes() == bsdata(other)
+    }
+}
+
+// ByteStr == [u8]
+impl Eq<[u8]> for ByteStr {
+    fn eq(&self, other: &[u8]) -> bool {
+        bsdata(self) == other
+    }
+}
+
+// [u8] == ByteStr
+impl Eq<ByteStr> for [u8] {
+    fn eq(&self, other: &ByteStr) -> bool {
+        self == bsdata(other)
+    }
+}
+
+// CStr == CStr
+impl Eq for CStr {
+    fn eq(&self, other: &CStr) -> bool {
+        csdata(self) == csdata(other)
+    }
+}
+
+// CStr == ByteStr
+impl Eq<ByteStr> for CStr {
+    fn eq(&self, other: &ByteStr) -> bool {
+        csnndata(self) == bsdata(other)
+    }
+}
+
+// CStr == NoNullStr
+impl Eq<NoNullStr> for CStr {
+    fn eq(&self, other: &NoNullStr) -> bool {
+        csnndata(self) == nnsdata(other)
+    }
+}
+
+// CStr == str
+impl Eq<str> for CStr {
+    fn eq(&self, other: &str) -> bool {
+        csnndata(self) == other.as_bytes()
+    }
+}
+
+// str == CStr
+impl Eq<CStr> for str {
+    fn eq(&self, other: &CStr) -> bool {
+        self.as_bytes() == csnndata(other)
+    }
+}
+
+// CStr == [u8]
+impl Eq<[u8]> for CStr {
+    fn eq(&self, other: &[u8]) -> bool {
+        csnndata(self) == other
+    }
+}
+
+// [u8] == CStr
+impl Eq<CStr> for [u8] {
+    fn eq(&self, other: &CStr) -> bool {
+        self == csnndata(other)
+    }
+}
+
+// NoNullStr == NoNullStr
+impl Eq for NoNullStr {
+    fn eq(&self, other: &NoNullStr) -> bool {
+        nnsdata(self) == nnsdata(other)
+    }
+}
+
+// NoNullStr == ByteStr
+impl Eq<ByteStr> for NoNullStr {
+    fn eq(&self, other: &ByteStr) -> bool {
+        nnsdata(self) == bsdata(other)
+    }
+}
+
+// NoNullStr == CStr
+impl Eq<CStr> for NoNullStr {
+    fn eq(&self, other: &CStr) -> bool {
+        nnsdata(self) == csnndata(other)
+    }
+}
+
+// NoNullStr == str
+impl Eq<str> for NoNullStr {
+    fn eq(&self, other: &str) -> bool {
+        nnsdata(self) == other.as_bytes()
+    }
+}
+
+// str == NoNullStr
+impl Eq<NoNullStr> for str {
+    fn eq(&self, other: &NoNullStr) -> bool {
+        self.as_bytes() == nnsdata(other)
+    }
+}
+
+// NoNullStr == [u8]
+impl Eq<[u8]> for NoNullStr {
+    fn eq(&self, other: &[u8]) -> bool {
+        nnsdata(self) == other
+    }
+}
+
+// [u8] == NoNullStr
+impl Eq<NoNullStr> for [u8] {
+    fn eq(&self, other: &NoNullStr) -> bool {
+        self == nnsdata(other)
+    }
+}
---
+++ src/str_two/cmp.rs
@@ -0,0 +1,81 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use core::ops::{Eq, Deref};
+use str_one::{ByteStr, CStr, NoNullStr};
+use string::{String};
+use byte_string::{ByteString};
+use c_string::{CString};
+use no_null_string::{NoNullString};
+use alloc::{Allocator};
+
+macro_rules! owned {
+    ($one:ident, $two:ident) => {
+        impl<'a, 'b, H1, H2> Eq<$two<'b, H2>> for $one<'a, H1>
+            where H1: Allocator,
+                  H2: Allocator,
+        {
+            fn eq(&self, other: &$two<'b, H2>) -> bool {
+                self.deref() == other.deref()
+            }
+        }
+    }
+}
+
+owned!(ByteString,   ByteString);
+owned!(ByteString,   NoNullString);
+owned!(ByteString,   CString);
+owned!(ByteString,   String);
+owned!(NoNullString, ByteString);
+owned!(NoNullString, NoNullString);
+owned!(NoNullString, CString);
+owned!(NoNullString, String);
+owned!(CString,      ByteString);
+owned!(CString,      NoNullString);
+owned!(CString,      CString);
+owned!(CString,      String);
+owned!(String,       ByteString);
+owned!(String,       NoNullString);
+owned!(String,       CString);
+owned!(String,       String);
+
+macro_rules! borrowed {
+    ($one:ident, $two:ty) => {
+        impl<'a, H> Eq<$two> for $one<'a, H>
+            where H: Allocator,
+        {
+            fn eq(&self, other: &$two) -> bool {
+                self.deref() == other
+            }
+        }
+        impl<'a, H> Eq<$one<'a, H>> for $two
+            where H: Allocator,
+        {
+            fn eq(&self, other: &$one<'a, H>) -> bool {
+                self == other.deref()
+            }
+        }
+    }
+}
+
+borrowed!(ByteString,   ByteStr);
+borrowed!(ByteString,   NoNullStr);
+borrowed!(ByteString,   CStr);
+borrowed!(ByteString,   str);
+borrowed!(ByteString,   [u8]);
+borrowed!(NoNullString, ByteStr);
+borrowed!(NoNullString, NoNullStr);
+borrowed!(NoNullString, CStr);
+borrowed!(NoNullString, str);
+borrowed!(NoNullString, [u8]);
+borrowed!(CString,      ByteStr);
+borrowed!(CString,      NoNullStr);
+borrowed!(CString,      CStr);
+borrowed!(CString,      str);
+borrowed!(CString,      [u8]);
+borrowed!(String,       ByteStr);
+borrowed!(String,       NoNullStr);
+borrowed!(String,       CStr);
+borrowed!(String,       str);
+borrowed!(String,       [u8]);
