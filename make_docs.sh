b4d57149ce29c6e2fd979baa0fc6cdc044979827
Julian Orth <ju.orth@gmail.com>
2015-11-11 17:46:53 +0100

    autocommit 2015-11-11 17:46:51 CET

--- make_docs.sh
+++ make_docs.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# rustdoc -L obj src/lrs/lib.rs
-./rustdoc -L obj src/lrs/lib.rs
-cd ../doc
-./lrs_doc
-# cp assets/doc_style.css doc/main.css
-# cd doc
-# rm *.woff
+rustdoc -w json -L obj src/lrs/lib.rs
+lrs_doc
+cp assets/doc_style.css doc/style.css
