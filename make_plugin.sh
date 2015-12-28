4751b79ab7a59130898cedd2b599ba6148518a3e
ilkosta <costantino.giuliodori@gmail.com>
2015-12-28 09:31:11 +0100

    fix [[ errors when the sh isn't bash

--- make_plugin.sh
+++ make_plugin.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 target="$(./targets.sh native)"
 
@@ -6,6 +6,7 @@ rustc -o obj/$target/liblrs_core_plugin.so src/core_plugin/lib.rs
 
 for t in $(./targets.sh all); do
     if [[ ! -e obj/$t/liblrs_core_plugin.so ]]; then
+	test -d obj/$t || mkdir obj/$t
         ln -s ../$target/liblrs_core_plugin.so obj/$t/liblrs_core_plugin.so
     fi
 done
